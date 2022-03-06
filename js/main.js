4400236f2240c7a1badd7e821f9d58d233da069e
HafizullahIT19 <tisudd19hafrah@gmail.com>
2022-03-06 21:06:39 +0100

    Learn Object [ Project 04]

--- js/main.js
+++ js/main.js
@@ -1,32 +1,21 @@
-// Project 03: obj1 === obj2 
+// Project 04: Create a Object Post
 
 
-var address1 = new CreateAddress('uddevalla', 24, '878374');
-var address2 = new CreateAddress('uddevalla', 24, '878374');
-
-console.log(areSame(address1, address2)); // false 
-console.log(areEqual(address1, address2));
-
-//------------------------------------------------------
-// constructor function
-function CreateAddress(city, street, zipCode){
-    this.city = city;
-    this.street = street;
-    this.zipCode = zipCode;
+var post = {
+    title: 'a',
+    body: 'd',
+    author: 'Hafiz',
+    comments: [
+        {author: 'Ali', text:'d'},
+        {author:'Hamid', text: 'd'},
+    ],
+    views: 354,
+    isLive: true,
 }
 
+console.log(post);
 
 
-function areSame(obj1, obj2) {
-    return obj1 === obj2;
-}
-
-
-function areEqual(obj1, obj2) {
-    return obj1.city === obj2.city && 
-    obj1.street === obj2.street && 
-    obj1.zipCode === obj2.zipCode
-}
 
 
 
--- txt/ECMAScript.md
+++ txt/ECMAScript.md
@@ -272,6 +272,7 @@ Template Literals (Back Tick )
 1. Project 01: [Video_12](https://megajs.com/?p=2402 "Megajs")
 2. Project 02: [Video_13](https://megajs.com/?p=2402 "Megajs")
 3. Project 03: [Video_14](https://megajs.com/?p=2402 "Megajs")
+4. Project 04: [Video_15](https://megajs.com/?p=2402 "Megajs")
 
 <br><br>
 
